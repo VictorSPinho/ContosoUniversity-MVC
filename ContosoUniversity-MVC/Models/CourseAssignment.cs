70f9cf23ab25f56e17b6e59ada7306c8c4717ec1
Victors01 <victorsantos4704@gmail.com>
2026-04-28 22:10:48 -0300

    feat: adiciona entidades Instructor, Department e OfficeAssignment; atualiza migrações e seed de dados

---
+++ ContosoUniversity-MVC/Models/CourseAssignment.cs
@@ -0,0 +1,16 @@
+﻿using ContosoUniversity_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity_MVC.Models
+{
+    public class CourseAssignment
+    {
+        public int InstructorID { get; set; }
+        public int CourseID { get; set; }
+        public Instructor Instructor { get; set; }
+        public Course Course { get; set; }
+    }
+}
--- ContosoUniversity-MVC/Models/Enrollment.cs
+++ ContosoUniversity-MVC/Models/Enrollment.cs
@@ -1,4 +1,6 @@
-﻿namespace ContosoUniversity_MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ContosoUniversity_MVC.Models
 {
     public enum Grade
     {
@@ -10,6 +12,7 @@
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
